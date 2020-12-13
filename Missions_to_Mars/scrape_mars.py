bb0c9775178ac9993f3d93e91399aee0b22a201f
zombimaki <andrewbperez@gmail.com>
2020-12-12 21:21:36 -0600

    icecream

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -8,135 +8,173 @@ import requests
 from splinter import Browser
 import time
 
-executable_path = {'executable_path': 'C:\ProgramData\ChromeDriver\chromedriver'}
-browser = Browser('chrome', **executable_path, headless=False)
+mars_scrape_dict = {}
 
 ################################################################################
 # NASA Mars News
 ################################################################################
+def scrape_mars_news():
 
-# url for scraping
-mars_news_url = 'https://mars.nasa.gov/news/'
-browser.visit(mars_news_url)
+        executable_path = {'executable_path': 'C:\ProgramData\ChromeDriver\chromedriver'}
+        browser = Browser('chrome', **executable_path, headless=False)
 
-# HTML object
-mars_news_html = browser.html
+        # url for scraping
+        mars_news_url = 'https://mars.nasa.gov/news/'
+        browser.visit(mars_news_url)
 
-# Parse HTML with Beautiful Soup
-mars_news_soup = bs(mars_news_html, 'html.parser')
+        # HTML object
+        mars_news_html = browser.html
 
-# wait 5 seconds before running next block
-time.sleep(5)
+        # Parse HTML with Beautiful Soup
+        mars_news_soup = bs(mars_news_html, 'html.parser')
 
-# collect the latest News Title and Paragraph Text
-mars_news_find = mars_news_soup.find("div", class_="list_text")
+        # wait 5 seconds before running next block
+        time.sleep(5)
 
-news_title = mars_news_find.find('div', class_='content_title').text
+        # collect the latest News Title and Paragraph Text
+        mars_news_find = mars_news_soup.find("div", class_="list_text")
 
-news_p = mars_news_find.find('div', class_='article_teaser_body').text 
+        news_title = mars_news_find.find('div', class_='content_title').text
+
+        news_p = mars_news_find.find('div', class_='article_teaser_body').text 
+
+        mars_scrape_dict['news_title'] = news_title
+        mars_scrape_dict['news_p'] = news_p
+
+        return mars_scrape_dict
+
+        browser.quit()
 
 ################################################################################
 # JPL Mars Space Images - Featured Image
 ################################################################################
+def scrape_jpl():
 
-# url for scraping
-jpl_base_url = 'https://www.jpl.nasa.gov/'
-jpl_suffix_url = 'spaceimages/?search=&category=Mars'
-jpl_img_url = jpl_base_url+jpl_suffix_url
+        executable_path = {'executable_path': 'C:\ProgramData\ChromeDriver\chromedriver'}
+        browser = Browser('chrome', **executable_path, headless=False)
 
-browser.visit(jpl_img_url)
+        # url for scraping
+        jpl_base_url = 'https://www.jpl.nasa.gov/'
+        jpl_suffix_url = 'spaceimages/?search=&category=Mars'
+        jpl_img_url = jpl_base_url+jpl_suffix_url
 
-# HTML object
-jpl_img_html = browser.html
+        browser.visit(jpl_img_url)
 
-# Parse HTML with Beautiful Soup
-jpl_img_soup = bs(jpl_img_html, 'html.parser')
+        # HTML object
+        jpl_img_html = browser.html
 
-# wait 5 seconds before running next block
-time.sleep(5)
+        # Parse HTML with Beautiful Soup
+        jpl_img_soup = bs(jpl_img_html, 'html.parser')
 
-# find the div class "carousel_container"
-jpl_img_find = jpl_img_soup.find("div", class_="carousel_container")
+        # wait 5 seconds before running next block
+        time.sleep(5)
 
-# find the img url in the ariticle class carousel_item in the style 
-image_url = jpl_img_find.find("article", class_="carousel_item")["style"].\
-        replace("'",'').\
-        replace(";",'').\
-        replace(")",'').\
-        replace("(",'').\
-        replace("background-image: url/",'')
+        # find the div class "carousel_container"
+        jpl_img_find = jpl_img_soup.find("div", class_="carousel_container")
 
-# concatenate the base and img urls
-featured_image_url = jpl_base_url + image_url
+        # find the img url in the ariticle class carousel_item in the style 
+        image_url = jpl_img_find.find("article", class_="carousel_item")["style"].\
+                replace("'",'').\
+                replace(";",'').\
+                replace(")",'').\
+                replace("(",'').\
+                replace("background-image: url/",'')
 
+        # concatenate the base and img urls
+        featured_image_url = jpl_base_url + image_url
 
+        mars_scrape_dict['image_url'] = featured_image_url
+
+        return mars_scrape_dict
+
+        browser.quit()
 ################################################################################
 # Mars Facts
 ################################################################################
+def scrape_facts():
 
-# declare the url for pandas to scrape
-mars_facts_url = 'https://space-facts.com/mars/'
+        executable_path = {'executable_path': 'C:\ProgramData\ChromeDriver\chromedriver'}
+        browser = Browser('chrome', **executable_path, headless=False)
 
-mars_facts_url_df = pd.read_html(mars_facts_url)
+        # declare the url for pandas to scrape
+        mars_facts_url = 'https://space-facts.com/mars/'
 
-# take the first table of metrics
-mars_clean_fact_df = mars_facts_url_df[0]
+        mars_facts_url_df = pd.read_html(mars_facts_url)
 
-# rename colums
-mars_clean_fact_df.columns = ["Description", "Mars"]
-mars_clean_fact_df = mars_clean_fact_df.set_index("Description")
+        # take the first table of metrics
+        mars_clean_fact_df = mars_facts_url_df[0]
 
-# generate an html file of the df to the Resources folder
-mars_clean_fact_df.to_html('Resources\mars_facts.html')
+        # rename colums
+        mars_clean_fact_df.columns = ["Description", "Mars"]
+        mars_clean_fact_df = mars_clean_fact_df.set_index("Description")
+
+        # generate an html file of the df to the Resources folder
+        html_table = mars_clean_fact_df.to_html()
+
+        mars_scrape_dict['html_table'] = html_table
+
+        return mars_scrape_dict
+
+        browser.quit()
 
 ################################################################################
 # Mars Hemispheres
 ################################################################################
+def scrape_hemi():
 
-# declare the urls
-mars_hemi_base_url = 'https://astrogeology.usgs.gov'
-mars_hemi_suffix_url = '/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+        executable_path = {'executable_path': 'C:\ProgramData\ChromeDriver\chromedriver'}
+        browser = Browser('chrome', **executable_path, headless=False)
 
-mars_hemi_url = mars_hemi_base_url+mars_hemi_suffix_url
+        # declare the urls
+        mars_hemi_base_url = 'https://astrogeology.usgs.gov'
+        mars_hemi_suffix_url = '/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
 
-browser.visit(mars_hemi_url)
+        mars_hemi_url = mars_hemi_base_url+mars_hemi_suffix_url
 
-# HTML object
-mars_hemi_html = browser.html
+        browser.visit(mars_hemi_url)
 
-# Parse HTML with Beautiful Soup
-mars_hemi_soup = bs(mars_hemi_html, 'html.parser')
+        # HTML object
+        mars_hemi_html = browser.html
 
-# wait 5 seconds before running next block
-time.sleep(5)
+        # Parse HTML with Beautiful Soup
+        mars_hemi_soup = bs(mars_hemi_html, 'html.parser')
 
-# query the item div class
-mars_hemi_find = mars_hemi_soup.find_all("div", class_="item")
+        # wait 5 seconds before running next block
+        time.sleep(5)
 
-# create a list to hold the dictionaries
-hemisphere_image_urls =[]
+        # query the item div class
+        mars_hemi_find = mars_hemi_soup.find_all("div", class_="item")
 
-for hemi in mars_hemi_find:
-    # grab the title in the h3 tags
-    title = hemi.find('h3').text
-    # grab the href 
-    hemi_img_suffix_url = hemi.find("a", class_="itemLink product-item")['href']
-    hemi_img_url = mars_hemi_base_url+hemi_img_suffix_url
-    
-    browser.visit(hemi_img_url)
+        # create a list to hold the dictionaries
+        hemisphere_image_urls =[]
 
-    # HTML object
-    hemi_html = browser.html
+        for hemi in mars_hemi_find:
+                # grab the title in the h3 tags
+                title = hemi.find('h3').text
+                # grab the href 
+                hemi_img_suffix_url = hemi.find("a", class_="itemLink product-item")['href']
+                hemi_img_url = mars_hemi_base_url+hemi_img_suffix_url
 
-    # Parse HTML with Beautiful Soup
-    hemi_soup = bs(hemi_html, 'html.parser')
-    
-    # search the new link for the downloads div class
-    hemi_find = hemi_soup.find("div", class_="downloads")
-    # grab the first href a class
-    img_url = hemi_find.find("a")["href"]
-    # append dict to list
-    hemisphere_image_urls.append({
-                                    "title": title, 
-                                    "img_url": img_url
-                                 })
+                browser.visit(hemi_img_url)
+
+                # HTML object
+                hemi_html = browser.html
+
+                # Parse HTML with Beautiful Soup
+                hemi_soup = bs(hemi_html, 'html.parser')
+
+                # search the new link for the downloads div class
+                hemi_find = hemi_soup.find("div", class_="downloads")
+                # grab the first href a class
+                img_url = hemi_find.find("a")["href"]
+                # append dict to list
+                hemisphere_image_urls.append({
+                                                "title": title, 
+                                                "img_url": img_url
+                                                })
+
+                mars_scrape_dict['hemis'] = hemisphere_image_urls
+
+        return mars_scrape_dict
+
+        browser.quit()
